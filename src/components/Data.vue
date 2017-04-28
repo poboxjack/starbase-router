3db972caba31dbe7cf032de888a9edbd04ae6778
Jack Lu <poboxjack@gmail.com>
2017-04-28 14:58:34 +0800

    develop data component

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="">
-  Type: {{type}}
+  {{items}}
 </div>
 </template>
 
@@ -8,15 +8,30 @@
 export default {
   data() {
     return {
-      type: this.$route.params.type
+      type: this.$route.params.type,
+      items: []
     }
   },
   watch: {
-    '$route': 'change'
+    '$route': 'fetchItems'
   },
   methods: {
-    change() {
-      this.type = this.$route.params.type
+    fetchItems() {
+      this.items = []
+      let initial_ids = [1, 13, 14]
+
+      for(let i in initial_ids) {
+        let id = initial_ids[i]
+        console.log('id', id)
+        fetch(`http://swapi.co/api/${this.type}/${id}`,{
+          method: 'GET'
+        })
+          .then(response => response.json())
+          .then(json => this.items.push(json))
+      }
+    },
+    created() {
+      this.fetchItems()
     }
   }
 }
